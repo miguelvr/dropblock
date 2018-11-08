936efa2ba5496353f6e0f9943d14df0cd71e432d
MiguelVR <miguelvramos@ist.utl.pt>
2018-11-08 01:00:45 +0100

    bump to version 0.2.0rc1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='dropblock',
-    version='0.1.0',
+    version='0.2.0rc1',
     packages=find_packages(),
     install_requires=required,
     url='https://github.com/miguelvr/dropblock',
